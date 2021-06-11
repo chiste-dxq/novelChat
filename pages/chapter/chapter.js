4fba781eddda10f01d1c81976339354d3e9659a3
daixq <80678738+chiste-dxq@users.noreply.github.com>
2021-06-11 17:43:41 +0800

    章节

---
+++ pages/chapter/chapter.js
@@ -0,0 +1,28 @@
+Page({
+  data: {
+    chapters:{},
+    novel_id:null
+  },
+  onLoad: function(options){
+    var that = this;
+    that.setData({
+      novel_id:options.novel_id
+    })
+    wx.request({
+      url: 'http://localhost:8930/api/novelContent/queryChapter?id='+that.data.novel_id,
+      header: {
+        'content-type': 'application/json',
+        'Token':'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VyQ29kZSI6ImFkbWluIn0.bSqexWHLzwANUQ_nSj14ojV33xD2UReU8-zcDAJxn4Q'
+      },
+      method: 'POST',//get或者post
+      data: { //请求参数，没有参数可以为空 
+      },
+      success: function (res) {
+        console.log(res)//接口返回值
+        that.setData({
+          chapters: res.data.data
+        })
+      }
+    })
+  },
+})
--- pages/index/index.js
+++ pages/index/index.js
@@ -6,8 +6,7 @@ Page({
    */
   data: {
     currentIndex: 0,
-    topNavs:{},
-    topNavsNovel:{},
+    topNavs:{}
   },
 
   onLoad: function(){
@@ -25,27 +24,6 @@ Page({
         console.log(res)//接口返回值
         that.setData({
           topNavs: res.data.data
-      })
-      }
-    })
-  },
-
-  getNovelListByCat: function(cat){
-    var that = this;
-    wx.request({
-      url: 'http://localhost:8930/api/novel/queryNovel',
-      header: {
-        'content-type': 'application/json',
-        'Token':'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VyQ29kZSI6ImFkbWluIn0.bSqexWHLzwANUQ_nSj14ojV33xD2UReU8-zcDAJxn4Q'
-      },
-      method: 'POST',//get或者post
-      data: { //请求参数，没有参数可以为空 
-        "catIds":cat
-      },
-      success: function (res) {
-        console.log(res)//接口返回值
-        that.setData({
-          topNavsNovel: res.data.data
       })
       }
     })
@@ -68,6 +46,10 @@ Page({
         //拿到当前索引并动态改变
         currentIndex: e.currentTarget.dataset.idx
       })
+  },
+  intoNovel: function (e){
+    wx.navigateTo({
+      url: '../chapter/chapter?novel_id='+e.currentTarget.dataset.id
+    })
   }
- 
 })
